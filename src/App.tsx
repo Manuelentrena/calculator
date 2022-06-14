bdf004e24707bbbeb9a98c5161e80fe97e6a86f0
Manuel Entrena <manuel.entrena@gmail.com>
2022-06-14 19:18:37 +0200

    Added namespaces in typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
 const sizes = ["XS", "S", "M", "L", "XL", "XXL"];
-import {client} from './interfaces/complejas';
-import {Persona} from './interfaces/classes';
 
-const Alicia = new Persona("Alicia",23,100);
-
-console.log(Alicia.comprar(22));
-
-console.log(client.getFullAdress())
 function App() {
   return (
     <div className="App">
